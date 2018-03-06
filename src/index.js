c0cf15f31d9e8a5b9e258c12bbe71bf8ed0e9cdf
SachokS <36642435+SachokS@users.noreply.github.com>
2018-03-06 21:11:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ module.exports = function solveSudoku(matrix)
 {
   var tr = [];
   var countt=0;
-  function Sudoku (in_val) {
+  function Sudoku (mat) {
 
   var solved = [];
   var st = 0;
 
-  insolved(in_val);
+  insolved(mat);
   solve();
-  function insolved(in_val) {
+  function insolved(mat) {
   st = 0;
   var sugg = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   for ( var i=0; i<9; i++) {
   solved[i] = [];
   for ( var j=0; j<9; j++ ) {
-  if ( in_val[i][j] ) {
-  solved[i][j] = [in_val[i][j], 'in', []];
+  if ( mat[i][j] ) {
+  solved[i][j] = [mat[i][j], 'in', []];
   }
   else {
   solved[i][j] = [0, 'unknown', sugg];
@@ -37,16 +37,16 @@ module.exports = function solveSudoku(matrix)
   }
   } while (changed);
 
-  if ( !isSolved() && !isFailed() ) {
+  if ( !iss() && !isf() ) {
 
   backtracking();
   }
   };
   function updatesuggs() {
   var changed = 0;
-  var b = strayDiff(solved[1][3][2], rowContent(1));
-  b = strayDiff(b, colContent(3));
-  b = strayDiff(b, sectContent(1, 3));
+  var b = strayDiff(solved[1][3][2], row(1));
+  b = strayDiff(b, col(3));
+  b = strayDiff(b, sect(1, 3));
   for ( var i=0; i<9; i++) {
   for ( var j=0; j<9; j++) {
   if ( 'unknown' != solved[i][j][1] ) {
@@ -64,40 +64,40 @@ module.exports = function solveSudoku(matrix)
   return changed;
   };
   function solveSingle(i, j) {
-  solved[i][j][2] = strayDiff(solved[i][j][2], rowContent(i));
-  solved[i][j][2] = strayDiff(solved[i][j][2], colContent(j));
-  solved[i][j][2] = strayDiff(solved[i][j][2], sectContent(i, j));
+  solved[i][j][2] = strayDiff(solved[i][j][2], row(i));
+  solved[i][j][2] = strayDiff(solved[i][j][2], col(j));
+  solved[i][j][2] = strayDiff(solved[i][j][2], sect(i, j));
   if ( 1 == solved[i][j][2].length ) {
   // Исключили все варианты кроме одного
-  markSolved(i, j, solved[i][j][2][0]);
+  mark(i, j, solved[i][j][2][0]);
   return 1;
   }
   return 0;
   };
   function solveHiddenSingle(i, j) {
-  var less_sugg = lessRowsugg(i, j);
+  var less = lessRow(i, j);
   var changed = 0;
-  if ( 1 == less_sugg.length ) {
-  markSolved(i, j, less_sugg[0]);
+  if ( 1 == less.length ) {
+  mark(i, j, less[0]);
   changed++;
   }
-  var less_sugg = lessColsugg(i, j);
-  if ( 1 == less_sugg.length ) {
-  markSolved(i, j, less_sugg[0]);
+  var less = lessCol(i, j);
+  if ( 1 == less.length ) {
+  mark(i, j, less[0]);
   changed++;
   }
-  var less_sugg = lessSectsugg(i, j);
-  if ( 1 == less_sugg.length ) {
-  markSolved(i, j, less_sugg[0]);
+  var less = lessSect(i, j);
+  if ( 1 == less.length ) {
+  mark(i, j, less[0]);
   changed++;
   }
   return changed;
   };
-  function markSolved(i, j, solve) {
+  function mark(i, j, solve) {
   solved[i][j][0] = solve;
   solved[i][j][1] = 'solved';
   };
-  function rowContent(i) {
+  function row(i) {
   var content = [];
   for ( var j=0; j<9; j++ ) {
   if ( 'unknown' != solved[i][j][1] ) {
@@ -106,7 +106,7 @@ module.exports = function solveSudoku(matrix)
   }
   return content;
   };
-  function colContent(j) {
+  function col(j) {
   var content = [];
   for ( var i=0; i<9; i++ ) {
   if ( 'unknown' != solved[i][j][1] ) {
@@ -115,7 +115,7 @@ module.exports = function solveSudoku(matrix)
   }
   return content;
   };
-  function sectContent(i, j) {
+  function sect(i, j) {
   var content = [];
   var offset = sectOffset(i, j);
   for ( var k=0; k<3; k++ ) {
@@ -127,39 +127,39 @@ module.exports = function solveSudoku(matrix)
   }
   return content;
   };
-  function lessRowsugg(i, j) {
-  var less_sugg = solved[i][j][2];
+  function lessRow(i, j) {
+  var less = solved[i][j][2];
   for ( var k=0; k<9; k++ ) {
   if ( k == j || 'unknown' != solved[i][k][1] ) {
   continue;
   }
-  less_sugg = strayDiff(less_sugg, solved[i][k][2]);
+  less = strayDiff(less, solved[i][k][2]);
   }
-  return less_sugg;
+  return less;
   };
-  function lessColsugg(i, j) {
-  var less_sugg = solved[i][j][2];
+  function lessCol(i, j) {
+  var less = solved[i][j][2];
   for ( var k=0; k<9; k++ ) {
   if ( k == i || 'unknown' != solved[k][j][1] ) {
   continue;
   }
-  less_sugg = strayDiff(less_sugg, solved[k][j][2]);
+  less = strayDiff(less, solved[k][j][2]);
   }
-  return less_sugg;
+  return less;
   };
-  function lessSectsugg(i, j) {
-  var less_sugg = solved[i][j][2];
+  function lessSect(i, j) {
+  var less = solved[i][j][2];
   var offset = sectOffset(i, j);
   for ( var k=0; k<3; k++ ) {
   for ( var l=0; l<3; l++ ) {
-  if ( ((offset.i+k) == i && (offset.j+l) == j)|| 'unknown' != solved[offset.i+k][offset.j+l][1] ) {
+  if (  'unknown' != solved[offset.i+k][offset.j+l][1] ) {
   continue;
   }
-  less_sugg = strayDiff(less_sugg, solved[offset.i+k][offset.j+l][2]);
+  less = strayDiff(less, solved[offset.i+k][offset.j+l][2]);
   }
   }
-  return less_sugg;
-  }; // end of method lessSectsugg()
+  return less;
+  }; // end of method lessSect()
 
   function strayDiff (ar1, ar2) {
   var str_diff = [];
@@ -168,7 +168,7 @@ module.exports = function solveSudoku(matrix)
   for ( var j=0; j<ar2.length; j++ ) {
   if ( ar1[i] == ar2[j] ) {
   is_found = true;
-  break;
+
   }
   }
   if ( !is_found ) {
@@ -198,7 +198,7 @@ module.exports = function solveSudoku(matrix)
   };
   };
 
-  function isSolved() {
+  function iss() {
   var is_solved = true;
   for ( var i=0; i<9; i++) {
   for ( var j=0; j<9; j++ ) {
@@ -210,10 +210,10 @@ module.exports = function solveSudoku(matrix)
   return is_solved;
   };
 
-  this.isSolved = function() {
-  return isSolved();
+  this.iss = function() {
+  return iss();
   };
-  function isFailed() {
+  function isf() {
   var is_failed = false;
   for ( var i=0; i<9; i++) {
   for ( var j=0; j<9; j++ ) {
@@ -222,23 +222,19 @@ module.exports = function solveSudoku(matrix)
   }
   }
   }
-  return is_failed;
   };
 
-  this.isFailed = function() {
-  return isFailed();
-  };
 
 
   function backtracking() {
   backtracking_call++;
-  var in_val = [[], [], [], [], [], [], [], [], []];
+  var mat = [[], [], [], [], [], [], [], [], []];
   var i_min=-1, j_min=-1, suggs_cnt=0;
   for ( var i=0; i<9; i++ ) {
-  in_val[i].length = 9;
+  mat[i].length = 9;
   for ( var j=0; j<9; j++ ) {
-  in_val[i][j] = solved[i][j][0];
-  if ( 'unknown' == solved[i][j][1] && (solved[i][j][2].length < suggs_cnt || !suggs_cnt) ) {
+  mat[i][j] = solved[i][j][0];
+  if ( 'unknown' == solved[i][j][1] && (!suggs_cnt) ) {
   suggs_cnt = solved[i][j][2].length;
   i_min = i;
   j_min = j;
@@ -248,16 +244,16 @@ module.exports = function solveSudoku(matrix)
 
 
   for ( var k=0; k<suggs_cnt; k++ ) {
-  in_val[i_min][j_min] = solved[i_min][j_min][2][k];
-  Sudoku(in_val);
-  if ( isSolved() ) {
+  mat[i_min][j_min] = solved[i_min][j_min][2][k];
+  Sudoku(mat);
+  if ( iss() ) {
 
-  out_val = isSolved();
+  out_val = iss();
 
   for ( var i=0; i<9; i++ ) {
   for ( var j=0; j<9; j++ ) {
   if ( 'unknown' == solved[i][j][1] ) {
-  markSolved(i, j, out_val[i][j][0])
+  mark(i, j, out_val[i][j][0])
   }
   }
   }
